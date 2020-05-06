e10b99435f3f9ac21d019ac7774bec4ca471269d
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-05-06 18:06:32 +0200

    fixed auto scaling

--- comet_longterm/utils.py
+++ comet_longterm/utils.py
@@ -28,6 +28,6 @@ def auto_unit(value, unit, decimals=3):
     if value is None:
         return "n/a"
     for scale, prefix in scales:
-        if value >= scale:
+        if abs(value) >= scale:
             return f"{value * (1 / scale):.{decimals}f} {prefix}{unit}"
     return f"{value:G} {unit}"
